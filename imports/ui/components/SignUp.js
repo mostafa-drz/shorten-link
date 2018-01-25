14d9f63903ba13784da2b8400f62e529c653e642
mostafa69d <mostafa69d@gmail.com>
2018-01-25 11:33:55 -0500

    feat: add signup

--- imports/ui/components/SignUp.js
+++ imports/ui/components/SignUp.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import { Accounts } from 'meteor/accounts-base'
+import Error from './helpers/Error';
 class SignUp extends Component {
-    render() {
-        return (
-          <p>Hello again</p>
-        );
-    }
+
+  constructor(props){
+    super(props);
+    this._handleEmailInput = this._handleEmailInput.bind(this);
+    this._handlePaswordInput = this._handlePaswordInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  state = {
+    error: false,
+    email: '',
+    password: ''
+  }
+
+  _handlePaswordInput(password){
+    this.setState({ password });
+  }
+
+  _handleEmailInput(email){
+    this.setState({ email });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const { email, password } = this.state;
+    Accounts.createUser( { email, password }, (error) => {
+      if(error){
+        console.log(error);
+        this.setState({ error: error.message });
+      }
+      else{
+        this.props.history.push('/links');
+      }
+    });
+  }
+
+  render() {
+    const { email, password, error } = this.state;
+    return (
+      <div>
+        <form onSubmit={(e) => this.handleSubmit(e) }>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" value={email} placeholder="email" onChange={(e)=>{this._handleEmailInput(e.target.value)}}/>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" value={password} placeholder="password" onChange={(e) => {this._handlePaswordInput(e.target.value)}} />
+          <button type="submit">Submit</button>
+          { error && <Error message={error}/> }
+        </form>
+        <Link to="/">Have An Account?</Link>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
