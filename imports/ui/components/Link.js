0441b89e7fd0cd27a165bf7d85b46f3fe8a69fe9
mostafa69d <mostafa69d@gmail.com>
2018-01-25 11:42:00 -0500

    feat: add logout

--- imports/ui/components/Link.js
+++ imports/ui/components/Link.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+ 
+class Link extends Component{
+  constructor(props){
+    super(props);
+    this.handleLogOut=this.handleLogOut.bind(this);
+  }
 
-const Link = () => {
+  handleLogOut(){
+    Meteor.logout();
+    this.props.history.push('/');
+  }
+  render(){
   return(
     <div>
       <p>Helloo from Link</p>
+      <button onClick={this.handleLogOut}>Log Out</button>
     </div>
   );
+}
 }
 
 export default Link;
