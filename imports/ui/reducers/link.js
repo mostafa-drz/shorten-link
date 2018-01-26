6d8bac7a9e10ff783cea18d824acfb8389f7f728
mostafa69d <mostafa69d@gmail.com>
2018-01-26 15:40:31 -0500

    fix: fix dupliation bug in links reducers

--- imports/ui/reducers/link.js
+++ imports/ui/reducers/link.js
@@ -3,7 +3,7 @@ import { ADD_LINKS } from '../actions/types';
 export default function listReducer(state = [], action) {
     switch (action.type) {
         case ADD_LINKS:
-            return state.concat(action.links);
+            return action.links;
         default:
             return state;
     }
