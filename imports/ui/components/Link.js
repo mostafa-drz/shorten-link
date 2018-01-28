72d0b6448bffde2fbd7ba7880b748192d86a7ac0
mostafa69d <mostafa69d@gmail.com>
2018-01-28 13:05:53 -0500

    feat: add prop types for validation

--- imports/ui/components/Link.js
+++ imports/ui/components/Link.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
 
-const Link = ({ url }) => {
+const Link = ({ url, _id }) => {
   return(
-      <li>{url}</li>
+      <li>
+      <p>{url}</p>
+      <a target='blank' href={Meteor.absoluteUrl(_id)}>{Meteor.absoluteUrl(_id)}</a>
+      </li>
   )
 }
 
+Link.propTypes = {
+  url: PropTypes.string.isRequired,
+  _id: PropTypes.string.isRequired
+}
 export default Link;
--- imports/ui/components/LinksList.js
+++ imports/ui/components/LinksList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Link from './Link';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 class LinksList extends Component{
   render(){
     return(
       <div>
         <ul>
-         { this.props.links.map((link)=>(<Link key={link._id} url={link.url} />))}
+         { this.props.links.map((link)=>(<Link key={link._id} url={link.url} _id={link._id} />))}
         </ul>
       </div>
     );
@@ -17,6 +19,10 @@ function mapStateToProps(state){
   return{
     links: state.links
   };
+}
+
+LinksList.propTypes = {
+  links: PropTypes.array.isRequired
 }
 
 export default connect(mapStateToProps)(LinksList);
