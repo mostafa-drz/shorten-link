ac2724c303c09434073e17da1c4f68d77610af75
mostafa69d <mostafa69d@gmail.com>
2018-01-29 14:11:02 -0500

    style: add style to not found page

--- imports/ui/components/NotFound.js
+++ imports/ui/components/NotFound.js
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
-    <p>Not found page</p>
+    <div className='not-found'>
+      <div className="not-found__container">
+        <h3 className='not-found__header'>404</h3>
+        <p className='not-found__description'>Page Not Found</p>
+        <Link to="/">Back To Home</Link>
+      </div>
+    </div>
+
   );
 }
 
